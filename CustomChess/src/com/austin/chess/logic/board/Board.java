c8ed94e6b98567c153a9e07e121d2ec5a453cdfa
Austin Malmberg <austin.malmberg@gmail.com>
2018-08-19 19:30:45 -0400

    Removed unused Board methods

--- CustomChess/src/com/austin/chess/logic/board/Board.java
+++ CustomChess/src/com/austin/chess/logic/board/Board.java
@@ -1,7 +1,6 @@
 package com.austin.chess.logic.board;
 
 import java.awt.Point;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -21,6 +20,8 @@ public class Board {
 	private List<Piece> pieces;
 	
 	private RelatedPoints relatedPoints;
+	
+	private Graveyard graveyard;
 
 	public Board(int boardstate, int ruleset) {
 		this.ruleset = new Ruleset(ruleset);
@@ -30,39 +31,22 @@ public class Board {
 		
 		relatedPoints = new RelatedPoints(this);
 		
-		// init pieces list
-		pieces = relatedPoints.allPointsOnBoardAsStream().map(point -> board[point.x][point.y]).filter(piece -> piece != null).collect(Collectors.toList());
+		graveyard = new Graveyard(PieceColor.values());
 		
-//		// update initial moves
+		// init pieces list
+		pieces = relatedPoints.allPointsOnBoardAsStream()
+				.map(this::getPiece)
+				.filter(piece -> piece != null)
+				.collect(Collectors.toList());
+		
+		// update initial moves
 //		pieces.stream().forEach(Piece::updateMoveset);
 	}
 	
 	// METHODS FOR PIECES
 	
-	public RelatedPoints relatedPoints() {
-		return relatedPoints;
-	}
-	
-	public List<Point> getFile(int c) {	// column
-		return relatedPoints.getFile(c);
-	}
-	
-	public List<Point> getRank(int r) {	// row
-		return relatedPoints.getRank(r);
-	}
-	
-	public boolean tileOccupied(Point p) {
+	public boolean isOccupied(Point p) {
 		return board[p.x][p.y] == null;
-	}
-	
-	public boolean isNullorEnemyAtLocation(PieceColor color, Point p) {
-		return tileOccupied(p) || isEnemyAtLocation(color, p);
-	}
-	
-	public boolean isEnemyAtLocation(PieceColor color, Point p) {
-		return !tileOccupied(p) &&
-				(color == PieceColor.WHITE && board[p.x][p.y].color() == PieceColor.BLACK ||
-						color == PieceColor.BLACK && board[p.x][p.y].color() == PieceColor.WHITE);
 	}
 	
 	public boolean inBounds(Point p) {
@@ -74,121 +58,60 @@ public class Board {
 	
 	public boolean tileAttackedByEnemy(PieceColor friendlyColor, Point p) {
 		return pieces.stream().filter(otherPiece -> 
-				otherPiece.color() != friendlyColor &&
-				otherPiece.validMoves().contains(p))
+				otherPiece.getColor() != friendlyColor &&
+				otherPiece.getValidMoves().contains(p))
 			.count() > 0;
 	}
 	
-	/**
-	 * 
-	 * @param color The color of the friendly piece
-	 * @param points A list of points
-	 * @return A sublist of points with the last point being either null or an enemy
-	 */
-	public List<Point> getAttackMoves(PieceColor color, List<Point> points) {
-		List<Point> attackMoves = new ArrayList<>();
-		for(Point p : points) {
-			if(tileOccupied(p)) {
-				Piece piece = board[p.x][p.y];
-				
-				if(piece.color() != color)
-					attackMoves.add(p);
-				
-				break;	
-			} else {
-				attackMoves.add(p);
-			}
+	public boolean tryMove(Point from, Point to) {
+		if(getPiece(from).getValidMoves().contains(to)) {
+			move(from, to);
+			return true;
 		}
 		
-		return attackMoves;
+		return false;
 	}
 	
-	public List<Point> getPassiveMoves(List<Point> points) {
-		List<Point> passiveMoves = new ArrayList<>();
-		for(Point p : points) {
-			if(tileOccupied(p))				
-				break;	
-			
-			passiveMoves.add(p);
-		}
+	private void move(Point from, Point to) {
+		Piece movingPiece = getPiece(from);
+		Piece capturedPiece = getPiece(to);
 		
-		return passiveMoves;
+		setPiece(from, null);
+		setPiece(to, movingPiece);
+		
+		movingPiece.move(to);
+		
+		updateCapturedPiece(capturedPiece);
 	}
 	
-	// METHODS FOR GAME
-	
-	public Piece[][] getBoard() {
-		return board;
+	public void updateCapturedPiece(Piece piece) {
+		if(piece == null) return;
+		
+		graveyard.add(piece.getColor(), piece.getType());
+		pieces.remove(piece);
 	}
-	
-	public Piece getPiece(Point p) {
-		return board[p.x][p.y];
-	}
-	
-	public Piece move(Point from, Point to) {
-		Piece movingPiece = board[from.x][from.y];
-		Piece vacatingPiece = board[to.x][to.y];
-		
-		if(vacatingPiece != null) {
-			pieces.remove(vacatingPiece);
-		}
-		
-		board[from.x][from.y] = null;
-		board[to.x][to.y] = movingPiece;
-		
-		movingPiece.setLocation(to);
-		
-		movingPiece.updateMoveset();
-		movingPiece.updateValidMoves();
-		
-		return vacatingPiece;
-	}
-	
-	public int countFriendlyPiecesOfSameType(Piece piece) {
-		return (int) pieces.stream().filter(p -> piece.color() == p.color() && piece.type() == p.type()).count();
-	}
-	
-//	public Piece move_returnCaptured(PieceColor color, Point from, Point to) {
-//		Piece capturedPiece = board[to.x][to.y];
-//		if(isValidMove(color, from, to)) {
-//			move(from, to);
-//			
-//			if(capturedPiece == null)
-//				return null;
-//			
-//			return capturedPiece;
-//		}
-//		
-//		// the piece did not move
-//		return null;
-//	}
-	
-//	public boolean isValidMove(PieceColor color, Point from, Point to) {
-//		boolean valid = tileOccupied(from) && sameColor(color, from) && board[from.x][from.y].attackMoves().contains(to);
-//		
-//		if(!tileOccupied(from))
-//			System.out.println("There isn't a piece there.");
-//		if(!sameColor(color, from))
-//			System.out.println("You can only move " + color.toString() + " pieces.");
-//		
-//		return valid;
-//	}
 	
 	public void updateValidMoves(PieceColor color) {
 		pieces.stream()
-				.filter(piece -> piece.color() == color)
+				.filter(piece -> piece.getColor() == color)
 				.forEach(Piece::updateValidMoves);
 	}
 	
-	// PRIVATE
+	// Getters -------------------------------------------------------------------------------------------------------
+
 	
-	private boolean sameColor(PieceColor color, Point p) {
-		return board[p.x][p.y].color() == color; 
+	public Piece getPiece(Point p) { return board[p.x][p.y]; }
+	
+	public RelatedPoints getRelatedPoints() { return relatedPoints; }
+	
+	public List<Point> getFile(int c) { return relatedPoints.getFile(c); }
+	public List<Point> getRank(int r) {	return relatedPoints.getRank(r); }
+	
+	// Setters -------------------------------------------------------------------------------------------------------
+	
+	public void setPiece(Point location, Piece piece) {
+		board[location.x][location.y] = piece;
 	}
-	
-	// PUBLIC GENERIC
-	
-	public List<Piece> getPieces() { return pieces; }
 	
 	public void print() {
 		for(int r = board.length-1; r >= 0; r--) {
